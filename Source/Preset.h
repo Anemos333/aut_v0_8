20e367965df5f54010a244ede3437123cbe57d29
Anemos333 <guglielmo.mihelj@gmail.com>
2026-06-27 18:24:04 +0200

    Add FactoryPreset struct definition
    Define FactoryPreset struct with various parameters.

--- Source/Preset.h
+++ Source/Preset.h
@@ -1,1 +1,30 @@
+#pragma once
 
+#include <vector>
+#include <string>
+#include <utility>
+
+struct FactoryPreset
+{
+    const char* name;
+    const char* group;
+
+    int processingMode; // 1 Quality, 2 Live, 3 Experimental
+
+    float speedMs;
+    float amount;
+    float humanize;
+
+    bool scaleLock;
+    float lockHysteresis;
+    float vibratoPreserve;
+
+    int tempoMode;       // 0 Off, 1 Tempo Glide, 2 Glide Lock
+    int tempoDivision;   // 0..4
+    float tempoGlidePct;
+    float tempoLockStrength;
+    bool tempoSmartOnset;
+
+    bool analogMode;
+    float outVolumeDb;
+};
